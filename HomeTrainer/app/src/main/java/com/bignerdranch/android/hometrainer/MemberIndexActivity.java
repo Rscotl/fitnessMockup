545b2a2fe80e85d3fc4d7fefd38b220298580069
Rscotl <ricky3693@hotmail.com>
2018-03-28 22:33:48 -0400

    mockup

---
+++ HomeTrainer/app/src/main/java/com/bignerdranch/android/hometrainer/MemberIndexActivity.java
@@ -0,0 +1,13 @@
+package com.bignerdranch.android.hometrainer;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MemberIndexActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_member_index);
+    }
+}
